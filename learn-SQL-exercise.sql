6d969447ce7d1861e0c6f1f7ee4d5568d260012f
Brian Pan <brianpanzifan@gmail.com>
2020-03-14 19:13:21 -0400

    1

--- learn-SQL-exercise.sql
+++ learn-SQL-exercise.sql
@@ -281,3 +281,20 @@ SELECT o.*,cust_name,cust_contact
 	WHERE C.cust_id = O.cust_id
 	AND OI.order_num = O.order_num
 	AND prod_id = 'RGAN01';
+    
+SELECT C.*, O.order_num, O.order_date, OI.prod_id, OI.quantity, OI.item_price
+	FROM Customers AS C, Orders AS O, OrderItems AS OI
+	WHERE C.cust_id = O.cust_id AND OI.order_num = O.order_num 
+	AND prod_id = 'RGAN01';
+    
+SELECT C.*, O.*
+	FROM Customers AS C, Orders AS O, OrderItems AS OI
+	WHERE C.cust_id = O.cust_id AND OI.order_num = O.order_num 
+	AND prod_id = 'RGAN01';
+
+SELECT C.cust_id, O.order_num
+FROM customers as C
+LEFT JOIN
+orders as O
+on C.cust_id=O.cust_id;
+
